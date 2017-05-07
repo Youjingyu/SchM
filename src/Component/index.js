c98554e162da71ed1223a5e9769e5a153ade1b91
whale <735284268@qq.com>
2017-05-07 21:55:17 +0800

    add grid component

--- src/Component/index.js
+++ src/Component/index.js
@@ -1,38 +1,28 @@
 import React, { Component } from 'react';
 import '../Style/app.scss';
-import { Flex, WhiteSpace } from 'antd-mobile';
+import { Grid } from 'antd-mobile';
+// import Grid from 'antd-mobile/lib/grid';
 
-let a = 1;
-if(a == '') {
-  a = 0;
-}
-const PlaceHolder = props => (
-    <div style={{
-      backgroundColor: '#ebebef',
-      color: '#bbb',
-      textAlign: 'center',
-      height: '0.6rem',
-      lineHeight: '0.6rem',
-      width: '100%'
-    }} {...props}
-    >Item</div>
-);
+const data1 = Array.from(new Array(5)).map((_val, i) => ({
+  img: 'https://zos.alipayobjects.com/rmsportal/wIjMDnsrDoPPcIV.png',
+  text: `name${i}`
+}));
 class App extends Component {
-  render () {
+  render() {
     return (
-            <div className="flex-container">
-                <div className="sub-title">基本</div>
-                <Flex>
-                    <Flex.Item><PlaceHolder /></Flex.Item>
-                    <Flex.Item><PlaceHolder /></Flex.Item>
-                </Flex>
-                <WhiteSpace size="lg" />
-                <Flex>
-                    <Flex.Item><PlaceHolder /></Flex.Item>
-                    <Flex.Item><PlaceHolder /></Flex.Item>
-                    <Flex.Item><PlaceHolder /></Flex.Item>
-                </Flex>
-            </div>
+    <div>
+      <div className="sub-title">custom content</div>
+      <Grid data={data1} columnNum={3} hasLine={false}
+            renderItem={(dataItem, index) => (
+              <div style={{ margin: '0.16rem', background: '#f7f7f7', textAlign: 'center' }}>
+                <div style={{ background: 'rgba(0, 0, 0, 0.1)', padding: '0.08rem' }}>
+                  <span>{index + 1}.{dataItem.text}</span>
+                </div>
+                <img src={dataItem.img} style={{ width: '80%', margin: '0.12rem' }} alt="" />
+              </div>
+            )}
+      />
+    </div>
     );
   }
 }
