e827b9a8b0ed11d6994d4a1e32d6e731fde4c706
youjingyu <735284268@qq.com>
2017-05-08 11:27:28 +0800

    add flexible.js

--- src/js/setRootFontSize.js
+++ src/js/setRootFontSize.js
@@ -1,19 +1,15 @@
-(function (doc, win) {
-  // let docEl = doc.documentElement;
-    // resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-  let docEl;
+export default function setRootFontSize (doc, win) {
+  let resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
   let recalc = function () {
-      // let docEl = doc.documentElement;
+    let docEl = doc.documentElement;
     let clientWidth = docEl.clientWidth;
     if (!clientWidth) return;
     docEl.style.fontSize = 12.5 * (clientWidth / 1200) + 'px';
   };
   if (doc.addEventListener) {
-    win.addEventListener('resize', recalc, false);
-    // doc.addEventListener('DOMContentLoaded', recalc, false);
+    win.addEventListener(resizeEvt, recalc, false);
   } else{
-    win.attachEvent('onresize', recalc);
-    // doc.attachEvent('onDOMContentLoaded', recalc);
+    win.attachEvent('on' + resizeEvt, recalc);
   }
   recalc();
-})(document, window);
+}
