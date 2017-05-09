15ab1bd5ec1af07431405490b066a82499369ea1
youjingyu <735284268@qq.com>
2017-05-09 17:53:07 +0800

    add touch event lib: react-hammerjs; add hammerjs to calendar

--- src/Component/calendar/calendar.js
+++ src/Component/calendar/calendar.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import CSSModules from 'react-css-modules';
 import Style from './calendar.scss';
+import Hammer from 'react-hammerjs';
 
-function computeDays () {
-  let date = new Date();
+function computeDays (date) {
+  date = date || new Date();
   let year = date.getFullYear(), month = date.getMonth();
   let isLeapYear = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
   let days = (month === 1) ? (28 + isLeapYear) : 31 - month % 7 % 2;
@@ -29,27 +30,46 @@ function computeDays () {
   return resultArr;
 }
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
   render() {
     const week = ['日', '一', '二', '三', '四', '五', '六'];
-    const days = computeDays();
+    const days = computeDays(this.state.date);
     return (
-    <div styleName="calendar">
-      <div styleName='cd-head'>
-        {week.map((val, index) => {
-          return <div key={index}>周{val}</div>;
+    <Hammer onSwipe={this.onSwipe}>
+      <div styleName="calendar">
+        <div styleName='cd-head'>
+          {week.map((val, index) => {
+            return <div key={index}>周{val}</div>;
+          })}
+        </div>
+        {days.map((row, index) => {
+          return (
+            <div styleName="cd-content" key={index}>
+              {row.map((day, index) => {
+                return <div key={index}>{day}</div>;
+              })}
+            </div>
+          );
         })}
       </div>
-      {days.map((row, index) => {
-        return (
-          <div styleName="cd-content" key={index}>
-            {row.map((day, index) => {
-              return <div key={index}>{day}</div>;
-            })}
-          </div>
-        );
-      })}
-    </div>
+    </Hammer>
     );
+  }
+  onSwipe = (event) => {
+    let date = this.state.date;
+    if(event.direction === 2) {
+      date.setMonth(date.getMonth() + 1);
+    } else {
+      date.setMonth(date.getMonth() - 1);
+    }
+    this.setState({
+      date: date
+    });
   }
 }
 
