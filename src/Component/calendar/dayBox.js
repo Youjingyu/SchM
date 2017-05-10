5d8d11b79515ddd5b51dc2450a4ebed673462bde
youjingyu <735284268@qq.com>
2017-05-10 11:27:49 +0800

    add DayBox component

---
+++ src/Component/calendar/dayBox.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import CSSModules from 'react-css-modules';
+import Hammer from 'react-hammerjs';
+import Style from './calendar.scss';
+
+class DayBox extends Component {
+  render() {
+    const data = this.props.data || [];
+    return (
+    <Hammer onTap={this.onTap}>
+      <div>
+        <span>{this.props.day}</span>
+        {data.map((val, index) => {
+          return <span key={index}>{val.theme}</span>;
+        })}
+      </div>
+    </Hammer>
+    );
+  }
+  onTap= () => {
+    console.log(1);
+  }
+}
+
+export default CSSModules(DayBox, Style);
