71203155404badda621e0ea15fe81ee6d9cf23c6
youjingyu <735284268@qq.com>
2017-06-12 17:52:54 +0800

    complete top menu style  30%

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Calendar from '../Component/calendar/calendar';
+import TopMenu from '../Component/topMenu/topMenu';
 import BottomMenu from '../Component/bottomMenu/bottomMenu';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <TopMenu />
         <Calendar />
         <BottomMenu />
       </div>
