ea566b08a2e3fae5daaa4591e3017fe4a52610ef
youjingyu <735284268@qq.com>
2017-06-14 17:14:20 +0800

    add datePiker

--- src/Pages/newSchedule.js
+++ src/Pages/newSchedule.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { DatePicker } from 'antd-mobile';
 import CSSModules from 'react-css-modules';
 import TopMenu from '../Component/topMenu/topMenuNewSch';
 import Style from '../Style/newSchedule.scss';
 
+const DateSelect = props => (
+  <div className="new-sch-time new-sch-start"
+       onClick={props.onClick}>
+    {props.children}
+    <span>{props.extra}</span>
+  </div>
+);
+
 class NewSchedule extends Component {
+  state = {
+    dpValue: null,
+    visible: false
+  }
   render() {
     return (
       <div styleName="new-sch">
@@ -13,7 +26,15 @@ class NewSchedule extends Component {
           <div styleName="new-sch-head">周末了，哈哈哈哈</div>
           <div styleName="new-sch-title">哈哈哈哈</div>
           <div styleName="new-sch-hr"></div>
-          <div styleName="new-sch-time new-sch-start"></div>
+          <DatePicker
+            mode="date"
+            title="选择日期"
+            extra="请选择(可选)"
+            value={this.state.dpValue}
+            onChange={v => this.setState({ dpValue: v })}
+          >
+            <DateSelect>日期(CST)</DateSelect>
+          </DatePicker>
           <div styleName="new-sch-time new-sch-end"></div>
         </div>
       </div>
