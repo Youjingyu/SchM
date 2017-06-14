5f35d33a0ef914b18bd2bd55182bd74b61acb623
youjingyu <735284268@qq.com>
2017-06-14 11:32:47 +0800

    complete schedule content style

--- src/Pages/newSchedule.js
+++ src/Pages/newSchedule.js
@@ -6,9 +6,16 @@ import Style from '../Style/newSchedule.scss';
 class NewSchedule extends Component {
   render() {
     return (
-      <div>
+      <div styleName="new-sch">
         <TopMenu />
         <input styleName="new-sch-input" placeholder="例如：明天上午九点开会" />
+        <div styleName="new-sch-content">
+          <div styleName="new-sch-head">周末了，哈哈哈哈</div>
+          <div styleName="new-sch-title">哈哈哈哈</div>
+          <div styleName="new-sch-hr"></div>
+          <div styleName="new-sch-time new-sch-start"></div>
+          <div styleName="new-sch-time new-sch-end"></div>
+        </div>
       </div>
     );
   }
