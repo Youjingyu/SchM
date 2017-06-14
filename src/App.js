a20f6e28e01b4ae953b32a545b8e8d5756ad7aab
youjingyu <735284268@qq.com>
2017-06-14 17:08:07 +0800

    cover the style of ant design datePicker

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Provider} from 'react-redux';
 import route from './Router/route'; // 路由配置
 import store from './Redux/Store/store';
 import './Style/common.scss';
+import './Style/coverAntDesign.css';
 
 ReactDOM.render(
     <Provider store={store}>
